6ff6cb49d28188b9e0861990ccb2d82a4adefcf5
mgismissing <gabrycraft2010@gmail.com>
2024-08-30 14:12:11 +0200

    Add key bruteforce

--- obsidian.py
+++ obsidian.py
@@ -1,15 +1,16 @@
-from cryptography.fernet import Fernet
+import cryptography.fernet
 import base64
 import hashlib
 import itertools
 import typing
+import time
 
 def encrypt(text: bytes, key: bytes) -> bytes:
-    fernet = Fernet(key)
+    fernet = cryptography.fernet.Fernet(key)
     return fernet.encrypt(text)
 
 def decrypt(text: bytes, key: bytes) -> bytes:
-    fernet = Fernet(key)
+    fernet = cryptography.fernet.Fernet(key)
     return fernet.decrypt(text)
 
 def key_from_password(password: bytes) -> bytes:
@@ -31,7 +32,14 @@ def shard_to_html(html: str, shard: str, key: bytes) -> int:
         html_file.write(decrypted)
     return 0
 
-def break_encryption(text: bytes, mode: typing.Literal["Aa", "0Aa", "%0Aa", "%0Aae", "<%0Aa", "<%0Aae"]) -> bytes:
+def break_encryption_shard_to_html(html: str, shard: str, mode: typing.Literal["Aa", "0Aa", "%0Aa", "%0Aae", "<%0Aa", "<%0Aae"], verbose: bool = False) -> int:
+    with open(f"{shard}.shard", "rb") as shard_file:
+        decrypted = break_encryption(shard_file.read(), mode, verbose)
+    with open(f"{html}.html", "wb") as html_file:
+        html_file.write(decrypted)
+    return 0
+
+def break_encryption(text: bytes, mode: typing.Literal["Aa", "0Aa", "%0Aa", "%0Aae", "<%0Aa", "<%0Aae"], verbose: bool = False) -> bytes:
     charset = []
     if mode == "Aa":
         charset = [chr(i) for i in range(0x41, 0x5A+1)] + [chr(i) for i in range(0x61, 0x7A+1)]
@@ -49,13 +57,15 @@ def break_encryption(text: bytes, mode: typing.Literal["Aa", "0Aa", "%0Aa", "%0A
     
     for length in range(1, 33):
         for combination in itertools.product(charset, repeat=length):
-            combos.append(''.join(combination))
-            print(combos[-1])
-            if combos[-1] == text:
-                return
+            combo = ''.join(combination)
+            if verbose: print(combo, end="\r")
+            try:
+                decrypted = decrypt(text, key_from_password(bytes(combo, "utf-8")))
+                if verbose: print(end="\n")
+                return decrypted
+            except cryptography.fernet.InvalidToken:
+                pass
 
 if __name__ == "__main__":
-    break_encryption("GAB", "%0Aa")
-    #print(key_from_password(b"greg"))
-    #html_to_shard("index", "index", key_from_password(b"greg"))
-    #shard_to_html("index", "index", key_from_password(b"greg"))
+    html_to_shard("index", "index", key_from_password(b"GREG"))
+    break_encryption_shard_to_html("index_decrypted", "index", "Aa", True)
